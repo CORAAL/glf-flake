da446bbe47bee27809ad074fdd4107654ea985cb
CORAAL <coraal@noreply.world>
2024-12-17 15:46:51 +0100

    fix

--- modules/glf.nix
+++ modules/glf.nix
@@ -1,20 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  options.hello = pkgs.lib.mkOption {
-    description = "Options for the hello package";
-    type = pkgs.lib.types.attrsOf (pkgs.lib.types.submodule {
-      options = {
-        enable = pkgs.lib.mkOption {
-          description = "Enable the hello package";
-          type = pkgs.lib.types.bool;
-          default = false;
-        };
-      };
-    });
+  options.hello.enable = pkgs.lib.mkOption {
+    description = "Enable the hello package";
+    type = pkgs.lib.types.bool;
+    default = false;
   };
 
-  config = pkgs.lib.mkIf config.hello.enable
-    { environment.systemPackages = with pkgs; [ hello ]; }
-    { environment.systemPackages = with pkgs; [ ]; };
+  config = pkgs.lib.mkIf config.hello.enable {
+    environment.systemPackages = with pkgs; [ hello ];
+  };
 }
