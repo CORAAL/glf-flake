a21f672bdf8c4771cce66d8e2ae881e5f21c83e8
CORAAL <coraal@noreply.world>
2024-12-17 16:15:32 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -5,24 +5,20 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
   outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs   = import nixpkgs { inherit system; }; 
-    in
-    {
-      nixosModules.default = { config, pkgs, ... }: {
+  {
+    nixosModules.default = { config, pkgs, ... }: {
 
-	options.hello.enable = pkgs.lib.mkOption {
-	    description = "Enable the hello package";
-	    type = pkgs.lib.types.bool;
-	    default = false;
-	};
+      options.hello.enable = pkgs.lib.mkOption {
+	description = "Enable the hello package";
+	type = pkgs.lib.types.bool;
+	default = false;
+      };
 
-	config = pkgs.lib.mkIf config.hello.enable {
-	    environment.systemPackages = with pkgs; [ hello ];
-	};
+      config = pkgs.lib.mkIf config.hello.enable {
+        environment.systemPackages = with pkgs; [ hello ];
       };
     };
+  };
 
   
 }
